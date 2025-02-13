a625b869b0e6663987ae6299784f3f7d938ced58
Xuân Huy <huylouis07@gmail.com>
2025-02-13 16:28:59 +0700

    UserPages

---
+++ StudentMind/Pages/UserPages/Delete.cshtml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using StudentMind.Core.Entity;
+using StudentMind.Infracstructure.Context;
+
+namespace StudentMind.Pages.UserPages
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly StudentMind.Infracstructure.Context.DatabaseContext _context;
+
+        public DeleteModel(StudentMind.Infracstructure.Context.DatabaseContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public User User { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var user = await _context.User.FirstOrDefaultAsync(m => m.Id == id);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                User = user;
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var user = await _context.User.FindAsync(id);
+            if (user != null)
+            {
+                User = user;
+                _context.User.Remove(User);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToPage("./Index");
+        }
+    }
+}

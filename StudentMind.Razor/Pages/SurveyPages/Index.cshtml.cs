8aaecf75bd060d074a45cec64b9ccf10c0a701e2
Thái Tiểu Bảo <thaitieubao2004@gmail.com>
2025-03-29 00:08:10 +0700

    generate code for survey

---
+++ StudentMind.Razor/Pages/SurveyPages/Index.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using StudentMind.Core.Entity;
+
+namespace StudentMind.Razor.Pages.SurveyPages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly StudentMind.Infrastructure.Context.DatabaseContext _context;
+
+        public IndexModel(StudentMind.Infrastructure.Context.DatabaseContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Survey> Survey { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            Survey = await _context.Surveys
+                .Include(s => s.Type).ToListAsync();
+        }
+    }
+}

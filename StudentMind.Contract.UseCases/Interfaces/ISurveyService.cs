dceb5555b18b8713f0a8fcb469cb8b7b0364f9bd
Thái Tiểu Bảo <thaitieubao2004@gmail.com>
2025-03-29 00:08:37 +0700

    add survey service

---
+++ StudentMind.Contract.UseCases/Interfaces/ISurveyService.cs
@@ -0,0 +1,19 @@
+﻿using StudentMind.Core.Entity;
+using StudentMind.Services.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentMind.Services.Interfaces
+{
+    public interface ISurveyService
+    {
+        Task<Survey> CreateSurvey(SurveyDTO surveyDto);
+        Task<Survey> UpdateSurvey(string id, SurveyDTO surveyDto);
+        Task DeleteSurvey(string id);
+        Task<Survey> GetSurveyById(string id);
+        Task<List<Survey>> GetSurveys();
+    }
+}
---
+++ StudentMind.Contract.UseCases/Services/SurveyService.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StudentMind.Core.Entity;
+using StudentMind.Core.Interfaces;
+using StudentMind.Services.DTO;
+using StudentMind.Services.Interfaces;
+
+namespace StudentMind.Services.Services
+{
+    public class SurveyService : ISurveyService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public SurveyService(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<Survey> CreateSurvey(SurveyDTO surveyDto)
+        {
+            var surveyRepo = _unitOfWork.GetRepository<Survey>();
+            var survey = new Survey
+            {
+                Id = Guid.NewGuid().ToString().ToUpper(),
+                Name = surveyDto.Name,
+                Description = surveyDto.Description,
+                StartDate = surveyDto.StartDate,
+                EndDate = surveyDto.EndDate,
+                TotalParticipants = surveyDto.TotalParticipants,
+                TypeId = surveyDto.TypeId
+            };
+            await surveyRepo.InsertAsync(survey);
+            await _unitOfWork.SaveAsync();
+            return survey;
+        }
+
+        public async Task<Survey> UpdateSurvey(string id, SurveyDTO surveyDto)
+        {
+            var surveyRepo = _unitOfWork.GetRepository<Survey>();
+            var survey = await surveyRepo.GetByIdAsync(id);
+            survey.Name = surveyDto.Name;
+            survey.Description = surveyDto.Description;
+            survey.StartDate = surveyDto.StartDate;
+            survey.EndDate = surveyDto.EndDate;
+            survey.TotalParticipants = surveyDto.TotalParticipants;
+            survey.TypeId = surveyDto.TypeId;
+            await surveyRepo.UpdateAsync(survey);
+            await _unitOfWork.SaveAsync();
+            return survey;
+        }
+
+        public async Task DeleteSurvey(string id)
+        {
+            var surveyRepo = _unitOfWork.GetRepository<Survey>();
+            await surveyRepo.DeleteAsync(id);
+            await _unitOfWork.SaveAsync();
+        }
+
+        public async Task<Survey> GetSurveyById(string id)
+        {
+            var surveyRepo = _unitOfWork.GetRepository<Survey>();
+            return await surveyRepo.GetByIdAsync(id);
+        }
+
+        public async Task<List<Survey>> GetSurveys()
+        {
+            var surveyRepo = _unitOfWork.GetRepository<Survey>();
+            return surveyRepo.Entities.Include(s => s.Type).ToList();
+        }
+    }
+}

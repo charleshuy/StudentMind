42b791e42c3ef3b0383a0e1ce82c2671bab339f5
Thái Tiểu Bảo <thaitieubao2004@gmail.com>
2025-03-29 03:08:10 +0700

    generate choice page and add to navbar

---
+++ StudentMind.Razor/Pages/ChoicePages/Edit.cshtml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using StudentMind.Core.Entity;
+using StudentMind.Infrastructure.Context;
+
+namespace StudentMind.Razor.Pages.ChoicePages
+{
+    public class EditModel : PageModel
+    {
+        private readonly StudentMind.Infrastructure.Context.DatabaseContext _context;
+
+        public EditModel(StudentMind.Infrastructure.Context.DatabaseContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Choice Choice { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var choice =  await _context.Choices.FirstOrDefaultAsync(m => m.Id == id);
+            if (choice == null)
+            {
+                return NotFound();
+            }
+            Choice = choice;
+           ViewData["QuestionId"] = new SelectList(_context.Questions, "Id", "Id");
+            return Page();
+        }
+
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more information, see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Attach(Choice).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ChoiceExists(Choice.Id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return RedirectToPage("./Index");
+        }
+
+        private bool ChoiceExists(string id)
+        {
+            return _context.Choices.Any(e => e.Id == id);
+        }
+    }
+}

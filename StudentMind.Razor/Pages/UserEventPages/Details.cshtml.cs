0d6cde58be50d8705ba0cb62ccea46d2d80411d5
somethinn <irtsomethinn@gmail.com>
2025-04-02 17:21:12 +0700

    create page to check all enrolled users of an event

--- StudentMind.Razor/Pages/UserEventPages/Details.cshtml.cs
+++ StudentMind.Razor/Pages/UserEventPages/Details.cshtml.cs
@@ -14,19 +14,24 @@ namespace StudentMind.Razor.Pages.UserEventPages
 
         public Event Event { get; set; }
         public bool IsJoined { get; set; }
+        public IEnumerable<User> JoinedUsers { get; set; }
+        public int CurrentPage { get; set; }
+        public int TotalPages { get; set; }
+        private const int PageSize = 5; 
 
         public DetailsModel(IUnitOfWork unitOfWork)
         {
             _unitOfWork = unitOfWork;
         }
 
-        public async Task<IActionResult> OnGetAsync(string id)
+        public async Task<IActionResult> OnGetAsync(string id, int page = 1)
         {
             if (string.IsNullOrEmpty(id))
             {
                 return NotFound();
             }
 
+            // Fetch the event
             var eventRepo = _unitOfWork.GetRepository<Event>();
             Event = await eventRepo.Entities
                 .Include(e => e.Host)
@@ -41,10 +46,27 @@ namespace StudentMind.Razor.Pages.UserEventPages
             var userId = GetCurrentUserId();
             if (!string.IsNullOrEmpty(userId))
             {
-                var userEventRepo = _unitOfWork.GetRepository<UserEvent>();
-                IsJoined = await userEventRepo.Entities
+                IsJoined = await _unitOfWork.GetRepository<UserEvent>().Entities
                     .AnyAsync(ue => ue.UserId == userId && ue.ProgramId == id && ue.DeletedTime == null);
             }
+
+            // Fetch the list of users who joined the event with pagination
+            var userEventRepo = _unitOfWork.GetRepository<UserEvent>();
+            var joinedUserEvents = userEventRepo.Entities
+                .Where(ue => ue.ProgramId == id && ue.DeletedTime == null)
+                .Include(ue => ue.User);
+
+            // Calculate pagination
+            var totalUsers = await joinedUserEvents.CountAsync();
+            TotalPages = (int)Math.Ceiling(totalUsers / (double)PageSize);
+            CurrentPage = page < 1 ? 1 : page > TotalPages ? TotalPages : page;
+
+            // Fetch the users for the current page
+            JoinedUsers = await joinedUserEvents
+                .Skip((CurrentPage - 1) * PageSize)
+                .Take(PageSize)
+                .Select(ue => ue.User)
+                .ToListAsync();
 
             return Page();
         }
